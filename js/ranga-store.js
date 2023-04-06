96e2ff3028b302995adc419cba1a80302772ab7d
ras1k <rasikabdullah@gmail.com>
2023-04-06 19:18:53 +0600

    image and price fixed

--- js/ranga-store.js
+++ js/ranga-store.js
@@ -1,189 +1,123 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>FakeStore</title>
-      <link rel="stylesheet" href="./css/style.css" />
-      <!-- Latest compiled and minified CSS -->
-      <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" /> -->
+const arr = [];
 
-      <!-- CSS only -->
-      <link
-         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-         rel="stylesheet"
-         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
-         crossorigin="anonymous"
-      />
+const loadProducts = (url) => {
+   fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+         arr.push(data);
+         showProducts(data);
+      });
+};
 
-      <link
-         rel="stylesheet"
-         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
-      />
+loadProducts('https://fakestoreapi.com/products');
 
-      <!-- google Montserrat  font  -->
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap"
-         rel="stylesheet"
-      />
-   </head>
+// show all product in UI
+const showProducts = (products) => {
+   
+   setInnerText('total_products', products.length);
 
-   <body>
-      <header>
-         <div class="row my-5 justify-content-center">
-            <div class="col-md-5">
-               <div class="input-group mb-3">
-                  <!-- need to add the input-group class to make it look ok -->
-                  <input
-                     type="text"
-                     class="form-control"
-                     placeholder="Search products"
-                     aria-label="Search Products"
-                     aria-describedby="basic-addon2"
-                     id="input-value"
-                  />
-                  <button
-                     class="input-group-text px-4 bg-success"
-                     id="search-btn"
-                  >
-                     <i class="bi bi-search text-white fw-bold"></i>
-                  </button>
-               </div>
-            </div>
-         </div>
-      </header>
-      <main in class="main">
-         <div class="my-5">
-            <h2 class="text-center">
-               Total Products:
-               <span class="text-success" id="total_products"></span>
-            </h2>
-         </div>
-         <div class="row">
-            <div class="col-md-9 col-lg-9 col-sm-7">
-               <div id="all-products" class="">
-                  <h1></h1>
-               </div>
-            </div>
+   document.getElementById("all-products").innerHTML = "";
 
-            <div class="col-md-3 col-lg-3 col-sm-5 cart-main">
-               <div class="cart" id="my-cart">
-                  <table class="table">
-                     <thead>
-                        <h1
-                           class="text-center text-white bg-success py-1 rounded"
-                        >
-                           My-Cart <i class="bi bi-cart4"></i>
-                        </h1>
-                        <tr>
-                           <th scope="col"></th>
-                           <th scope="col"></th>
-                           <th scope="col"></th>
-                           <th scope="col"></th>
-                        </tr>
-                     </thead>
-                     <tbody>
-                        <tr>
-                           <th>Total Added-Products:</th>
-                           <!-- here will be no dollar sign -->
-                           <td>$<span id="total-Products">0</span></td>
-                           <td></td>
-                           <td></td>
-                        </tr>
-                        <tr>
-                           <th>Price:</th>
-                           <td>$ <span id="price">0</span></td>
-                           <td></td>
-                           <td></td>
-                        </tr>
-                        <tr>
-                           <th>Delivery-Charge:</th>
-                           <td>$ <span id="delivery-charge">20</span></td>
-                           <td></td>
-                           <td></td>
-                        </tr>
-                        <tr>
-                           <th>Total-Tax:</th>
-                           <td>$ <span id="total-tax">0</span></td>
-                           <td></td>
-                           <td></td>
-                        </tr>
-                        <tr>
-                           <th scope="row">Total</th>
-                           <td colspan="2">$ <span id="total">0</span></td>
-                           <td></td>
-                        </tr>
-                     </tbody>
-                  </table>
-                  <div class="parces text-center">
-                     <button class="btn btn-success text-center">
-                        Buy Now
-                     </button>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </main>
-      <footer></footer>
-      <!-- modal section -->
-
-      <!-- Modal -->
-      <div
-         class="modal fade"
-         id="exampleModal"
-         tabindex="-1"
-         aria-labelledby="exampleModalLabel"
-         aria-hidden="true"
-      >
-         <div class="modal-dialog">
-            <div class="modal-content">
-               <div class="modal-header">
-                  <h5 class="modal-title text-success" id="exampleModalLabel">
-                     Modal title
-                  </h5>
-                  <button
-                     type="button"
-                     class="btn-close"
-                     data-bs-dismiss="modal"
-                     aria-label="Close"
-                  ></button>
-               </div>
-               <div class="ms-3 my-3">
-                  <strong> Product id: <span id="productId"></span> </strong>
-               </div>
-
-               <div class="modal-body text-secondary" id="modal_body"></div>
-               <div class="p-4 d-flex">
-                  <strong class="me-2">Rating</strong>
-                  <span
-                     id="rating"
-                     class="text-white p-2 circle"
-                  ></span>
-               </div>
-               <div class="modal-footer">
-                  <button
-                     type="button"
-                     class="btn btn-secondary"
-                     data-bs-dismiss="modal"
-                  >
-                     Close
-                  </button>
-               </div>
-            </div>
-         </div>
+   const allProducts = products.slice(0, 10).map((pd) => pd);
+   for (const product of allProducts) {
+      // console.log(product)
+      const image = product.image;
+      const div = document.createElement('div');
+      div.classList.add('product');
+      div.innerHTML = `<div class="single-product">
+      <div>
+    <img class="product-image" src=${image}></img>
       </div>
+      <h3>${product.title}</h3>
+      <p>Category: ${product.category}</p>
+      <h2>Price: $ ${product.price}</h2>
 
-      <!-- Bootstrap javascript  file  -->
-      <script
-         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-         crossorigin="anonymous"
-      ></script>
-      <!-- Custom javascript file -->
-      <script src="./js/app.js"></script>
-   </body>
-</html>
+      <button onclick="showProductDetails(${product.id})" id="details-btn"    data-bs-toggle="modal"
+      data-bs-target="#exampleModal" class="btn btn-outline-secondary mb-2 rounded-1 mt-1">Details</button>
+      
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success border-0 w-100 rounded-0 bg-main py-2">Add to cart</button>
+      `;
+      document.getElementById('all-products').appendChild(div);
+   }
+};
+
+let count = 0;
+
+const addToCart = (id, price) => {
+   count = count + 1;
+   updatePrice('price', price);
+   // debugger;
+   updateTaxAndCharge();
+   document.getElementById('total-Products').innerText = count;
+};
+
+const showProductDetails = (product_id) => {
+   console.log(product_id);
+   fetch(`https://fakestoreapi.com/products/${product_id}`)
+      .then((res) => res.json())
+      .then((data) => showProductDetailsInModal(data));
+};
+
+const showProductDetailsInModal = (product_details) => {
+   console.log(product_details);
+   setInnerText('exampleModalLabel', product_details.title);
+   setInnerText('product_id', product_details.id);
+   setInnerText('modal_body', product_details.description);
+   setInnerText('rating', product_details.rating.rate);
+};
+
+const getInputValue = (id) => {
+   const element = document.getElementById(id).innerText;
+   const converted = parseFloat(element);
+   return converted;
+};
+
+// main price update function
+const updatePrice = (id, value) => {
+   const convertedOldPrice = getInputValue(id);
+   const convertPrice = parseFloat(value);
+   const total = convertedOldPrice + convertPrice;
+   document.getElementById(id).innerText = total.toFixed(2);
+};
+
+// set innerText function
+const setInnerText = (id, value) => {
+   document.getElementById(id).innerText = Math.round(value);
+};
+
+// update delivery charge and total Tax
+const updateTaxAndCharge = () => {
+   const priceConverted = getInputValue('price');
+   if (priceConverted > 200) {
+      setInnerText('delivery-charge', 30);
+      setInnerText('total-tax', priceConverted * 0.2);
+   }
+   if (priceConverted > 400) {
+      setInnerText('delivery-charge', 50);
+      setInnerText('total-tax', priceConverted * 0.3);
+   }
+   if (priceConverted > 500) {
+      setInnerText('delivery-charge', 60);
+      setInnerText('total-tax', priceConverted * 0.4);
+   }
+};
+
+//grandTotal update function
+const updateTotal = () => {
+   const grandTotal =
+      getInputValue('price') +
+      getInputValue('delivery-charge') +
+      getInputValue('total-tax');
+   document.getElementById('total').innerText = grandTotal;
+};
+
+// search by category
+document.getElementById("search-btn").addEventListener("click", function () {
+   const inputField = document.getElementById("input-value").value;
+   const searchedProduct = arr[0].find((p) =>
+     p.category.startsWith(`${inputField}`)
+   );
+   showProducts(searchedProduct);
+ });
+
+
